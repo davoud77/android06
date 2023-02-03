c0ff7c22a31237bdb2f4a8da8d7a72e990bcce93
davoud77 <davoud.fathi1377@gmail.com>
2023-02-03 19:16:53 +0330

    add fun main

--- app/src/main/java/com/example/git006/MainActivity.kt
+++ app/src/main/java/com/example/git006/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.git006
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main()
+    }
+
+    private fun main() {
+        val name = "davoud"
+        val age = 1500
+
+        Toast.makeText(this, "$name and $age", Toast.LENGTH_SHORT).show()
     }
 }
